8a6e6c71d73d47e355924fe60be3fd72f14f31d9
rajput-karsh <utkarsh1@appinventiv.com>
2023-12-14 14:03:06 +0530

    add trace for error

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { toast } from "sonner";
-import { useState, useTransition, useRef, ElementRef } from "react";
-import { AlertTriangle } from "lucide-react";
-import { IngressInput } from "livekit-server-sdk";
+import { toast } from 'sonner';
+import { useState, useTransition, useRef, ElementRef } from 'react';
+import { AlertTriangle } from 'lucide-react';
+import { IngressInput } from 'livekit-server-sdk';
 
-import { createIngress } from "@/actions/ingress";
-import { Button } from "@/components/ui/button";
+import { createIngress } from '@/actions/ingress';
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogClose,
@@ -14,19 +14,15 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert";
+} from '@/components/ui/dialog';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from '@/components/ui/select';
 
 const RTMP = String(IngressInput.RTMP_INPUT);
 const WHIP = String(IngressInput.WHIP_INPUT);
@@ -34,27 +30,29 @@ const WHIP = String(IngressInput.WHIP_INPUT);
 type IngressType = typeof RTMP | typeof WHIP;
 
 export const ConnectModal = () => {
-  const closeRef = useRef<ElementRef<"button">>(null);
+  const closeRef = useRef<ElementRef<'button'>>(null);
   const [isPending, startTransition] = useTransition();
   const [ingressType, setIngressType] = useState<IngressType>(RTMP);
 
   const onSubmit = () => {
     startTransition(() => {
+      console.log('hereeee');
       createIngress(parseInt(ingressType))
         .then(() => {
-          toast.success("Ingress created");
+          toast.success('Ingress created');
           closeRef?.current?.click();
         })
-        .catch(() => toast.error("Something went wrong"));
+        .catch((err) => {
+          console.trace('Error in creating ingress - ', err);
+          toast.error('Something went wrong');
+        });
     });
-  }
+  };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="primary">
-          Generate connection
-        </Button>
+        <Button variant="primary">Generate connection</Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
@@ -77,20 +75,15 @@ export const ConnectModal = () => {
           <AlertTriangle className="h-4 w-4" />
           <AlertTitle>Warning!</AlertTitle>
           <AlertDescription>
-            This action will reset all active streams using the current connection
+            This action will reset all active streams using the current
+            connection
           </AlertDescription>
         </Alert>
         <div className="flex justify-between">
           <DialogClose ref={closeRef} asChild>
-            <Button variant="ghost">
-              Cancel
-            </Button>
+            <Button variant="ghost">Cancel</Button>
           </DialogClose>
-          <Button
-            disabled={isPending}
-            onClick={onSubmit}
-            variant="primary"
-          >
+          <Button disabled={isPending} onClick={onSubmit} variant="primary">
             Generate
           </Button>
         </div>
