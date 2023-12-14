c4583a815985a6158a4a99183e25a96ef485adef
rajput-karsh <utkarsh1@appinventiv.com>
2023-12-14 10:33:37 +0530

    fix liveKit Webhook

--- app/api/webhooks/livekit/route.ts
+++ app/api/webhooks/livekit/route.ts
@@ -1,7 +1,7 @@
-import { headers } from "next/headers";
-import { WebhookReceiver } from "livekit-server-sdk";
+import { headers } from 'next/headers';
+import { WebhookReceiver } from 'livekit-server-sdk';
 
-import { db } from "@/lib/db";
+import { db } from '@/lib/db';
 
 const receiver = new WebhookReceiver(
   process.env.LIVEKIT_API_KEY!,
@@ -11,15 +11,15 @@ const receiver = new WebhookReceiver(
 export async function POST(req: Request) {
   const body = await req.text();
   const headerPayload = headers();
-  const authorization = headerPayload.get("Authorization");
+  const authorization = headerPayload.get('Authorization');
 
   if (!authorization) {
-    return new Response("No authorization header", { status: 400 });
+    return new Response('No authorization header', { status: 400 });
   }
 
   const event = receiver.receive(body, authorization);
 
-  if (event.event === "ingress_started") {
+  if (event.event === 'ingress_started') {
     await db.stream.update({
       where: {
         ingressId: event.ingressInfo?.ingressId,
@@ -30,7 +30,7 @@ export async function POST(req: Request) {
     });
   }
 
-  if (event.event === "ingress_ended") {
+  if (event.event === 'ingress_ended') {
     await db.stream.update({
       where: {
         ingressId: event.ingressInfo?.ingressId,
@@ -40,4 +40,6 @@ export async function POST(req: Request) {
       },
     });
   }
+
+  return new Response('', { status: 200 });
 }
--- components/stream-player/header.tsx
+++ components/stream-player/header.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client';
 
-import { UserIcon } from "lucide-react";
-import { 
-  useParticipants, 
-  useRemoteParticipant
-} from "@livekit/components-react";
+import { UserIcon } from 'lucide-react';
+import {
+  useParticipants,
+  useRemoteParticipant,
+} from '@livekit/components-react';
 
-import { Skeleton } from "@/components/ui/skeleton";
-import { VerifiedMark } from "@/components/verified-mark";
-import { UserAvatar, UserAvatarSkeleton } from "@/components/user-avatar";
+import { Skeleton } from '@/components/ui/skeleton';
+import { VerifiedMark } from '@/components/verified-mark';
+import { UserAvatar, UserAvatarSkeleton } from '@/components/user-avatar';
 
-import { Actions, ActionsSkeleton } from "./actions";
+import { Actions, ActionsSkeleton } from './actions';
 
 interface HeaderProps {
   imageUrl: string;
@@ -19,7 +19,7 @@ interface HeaderProps {
   viewerIdentity: string;
   isFollowing: boolean;
   name: string;
-};
+}
 
 export const Header = ({
   imageUrl,
@@ -31,6 +31,10 @@ export const Header = ({
 }: HeaderProps) => {
   const participants = useParticipants();
   const participant = useRemoteParticipant(hostIdentity);
+
+  console.log(`participants -- `, participants);
+  console.log(`hostIdentity -- `, hostIdentity);
+  console.log(`participant -- `, participant);
 
   const isLive = !!participant;
   const participantCount = participants.length - 1;
@@ -50,19 +54,16 @@ export const Header = ({
         />
         <div className="space-y-1">
           <div className="flex items-center gap-x-2">
-            <h2 className="text-lg font-semibold">
-              {hostName}
-            </h2>
+            <h2 className="text-lg font-semibold">{hostName}</h2>
             <VerifiedMark />
           </div>
-          <p className="text-sm font-semibold">
-            {name}
-          </p>
+          <p className="text-sm font-semibold">{name}</p>
           {isLive ? (
-            <div className="font-semibold flex gap-x-1 items-center text-xs text-rose-500"> 
+            <div className="font-semibold flex gap-x-1 items-center text-xs text-rose-500">
               <UserIcon className="h-4 w-4" />
               <p>
-                {participantCount} {participantCount === 1 ? "viewer" : "viewers"}
+                {participantCount}{' '}
+                {participantCount === 1 ? 'viewer' : 'viewers'}
               </p>
             </div>
           ) : (
